e8d6b18ca69c8d874b9cc8c2d2a0c52455e08be6
bekadev <bekapng@gmail.com>
2022-09-01 21:53:07 +0600

    01.09.2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './component/Todolist';
+import {v1} from 'uuid';
 
 export type FilteredType = 'all' | 'completed' | 'active'
 
 function App() {
 
     let [tasks, setTasks] = useState([
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "TS", isDone: true },
-        { id: 4, title: "Angular", isDone: false },
-        { id: 5, title: "ReactJS", isDone: false }
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "TS", isDone: true }
     ])
-    const removeTask = (id: number) => {
+    const removeTask = (id: string) => {
         let filteredTasks = tasks.filter(task => task.id !== id)
         setTasks(filteredTasks)
     }
@@ -34,9 +33,31 @@ function App() {
         setFilter(value)
     }
 
+    const addTasks = (title: string) => {
+        let task = { id: v1(), title: title, isDone: true }
+        let newTasks = [task, ...tasks]
+        setTasks(newTasks)
+    }
+
+    const changeTaskStatus = (id: string, isDone: boolean) => {
+        let task = tasks.find(t => t.id === id)
+        if (task) {
+            task.isDone = isDone;
+            setTasks([...tasks])
+        }
+    }
+
     return (
         <div className="App">
-            <Todolist title={'City list'} tasks={tasksForTodolists} removeTask={removeTask} changeFilter={changeFilter}/>
+            <Todolist
+                title={'City list'}
+                tasks={tasksForTodolists}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTasks={addTasks}
+                changeTaskStatus={changeTaskStatus}
+                filter={filter}
+            />
         </div>
     );
 }
